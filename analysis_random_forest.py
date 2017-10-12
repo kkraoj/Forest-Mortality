421839e36771cf16cb68ffc0e336703c8d9fad54
kkraoj <kkraoj@gmail.com>
2017-10-11 23:31:52 -0700

    rf model updates

--- analysis_random_forest.py
+++ analysis_random_forest.py
@@ -5,6 +5,7 @@ Created on Tue Sep 26 18:32:07 2017
 @author: kkrao
 """
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,7 +13,7 @@ from dirs import Dir_CA
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import export_graphviz
 from sklearn import tree
-def rf_cleanup(year_range,*dfs):
+def rf_assemble(year_range,*dfs):
     if len(dfs)==1:
         dfs=[dfs]
 #    df=store['cwd']
@@ -21,11 +22,14 @@ def rf_cleanup(year_range,*dfs):
     for df in dfs:                    
         df=df[(df.index.year>=year_range[0]) &\
                   (df.index.year<=year_range[-1])].T
-        df.fillna(method='ffill',inplace=True)
-        df.fillna(method='bfill',inplace=True)
         array=pd.Series(df.values.flatten(),name=df.columns.name)
         out[array.name]=array
     return(out)
+
+def rf_fill(df):
+    df.fillna(method='ffill',inplace=True)
+    df.fillna(method='bfill',inplace=True)
+    return df
 
 input_sources=['mortality_025_grid','BPH_025_grid','LAI_025_grid_sum',\
 'LAI_025_grid_win','RWC', 'aspect_mean', 'aspect_std', 'canopy_height',\
@@ -47,19 +51,34 @@ store=pd.HDFStore(Dir_CA+'/data.h5')
 inputs=range(len(input_sources))
 for i in range(len(input_sources)):
     inputs[i]=store[input_sources[i]]
-Df=rf_cleanup(year_range,*inputs)
-Df.to_csv('D:/Krishna/Project/data/rf_data.csv')
+Df=rf_assemble(year_range,*inputs)
+Df['missing_data']=Df.T.isnull().sum()
+Df.loc[Df['missing_data']>=1,'missing_data']='yes'
+#Df.loc[Df['missing_data']==1,'missing_data']='yes'
+Df.loc[Df['missing_data']==0,'missing_data']='no'
 #-----------------------------------------------------------------------------
-Null=Df.isnull().mean()
-fig,ax=plt.subplots(figsize=(3,3))
-sns.barplot(y=Null.index,x=Null.values,ax=ax,color='purple')
-plt.tick_params(axis='y', which='major', labelsize=7)
-ax.set_xlabel('Fraction of Nulls')
+Null=Df[['RWC','cwd','vsm_sum','vsm_win']]
+Null=Null.isnull()
+Null.replace(True,np.nan,inplace=True)
+for column in Null.columns:
+    Null[column].replace(0.0,Null.columns.get_loc(column),inplace=True)
+Null['union']=Null.notnull().T.sum()
+Null.union[Null.overall!=4]=np.nan
+Null.union[Null.overall==4]=4
+fig,ax=plt.subplots(figsize=(6,3))
+Null.plot(linestyle='',marker='|',mew='0.1',markersize=10,ax=ax,legend=False,color='darkblue')
+#plt.tick_params(axis='y', which='major', labelsize=7)
+#ax.set_xlabel('Fraction of Nulls')
 ax.set_ylabel('Features')
-ax.set_title('Nulls in features')
+ax.set_title('Data Availability')
+plt.yticks(range(len(Null.columns)),Null.columns)
+labels=range(2009,2016)
+plt.xticks(np.linspace(0,len(Null.index),len(labels),endpoint=False),labels)
 
-#plt.xticks(rotation=45)
 
-for df in inputs:
-    print(df.index.name,df.index.year.min())
-
+Null.overall.replace(False,np.nan,inplace=True)
+Null_frac=(Null.missing_data==0).astype(int).mean()
+print('Union of Nulls = %0.2f'%Null_frac)
+#-----------------------------------------------------------------------------
+#Df=rf_fill(Df)
+#Df.to_csv('D:/Krishna/Project/data/rf_data.csv')
